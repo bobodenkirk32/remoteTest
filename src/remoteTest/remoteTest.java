15a7a10960e1bf1b60a79f5a43dcabe15e9bafbc
lew <bit4math@gmail.com>
2024-05-06 18:16:05 +0900

    second

--- src/remoteTest/remoteTest.java
+++ src/remoteTest/remoteTest.java
@@ -3,6 +3,8 @@ package remoteTest;
 public class remoteTest {
 	public static void main(String[] args) {
 		System.out.println("remoteTest");
+		System.out.println("remoteTest2");
+		System.out.println("remoteTest3");
 	}
 
 }
